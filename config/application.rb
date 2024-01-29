407e352a330b4786c614e6bc241dc8bb48c346be
uroojiqbal <sapna.iqbal@devsinc.com>
2024-01-29 16:07:37 +0500

    added tailwind

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module OnlineAuctionSystem
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.active_record.writing_role = :default
+    config.active_record.reading_role = :readonly
+
 
     # Configuration for the application, engines, and railties goes here.
     #
